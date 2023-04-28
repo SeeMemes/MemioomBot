3fcaa06e56dd57c633a24fd71d74de7549bb23a4
SeeMemes <provotorov-02@mail.ru>
2023-04-28 18:16:20 +0300

    finished UserRepo. Shall start annotation development

--- src/main/java/memioombot/backend/database/repositories/UserRepository.java
+++ src/main/java/memioombot/backend/database/repositories/UserRepository.java
@@ -16,7 +16,9 @@ import tech.ydb.table.transaction.TxControl;
 import tech.ydb.table.values.PrimitiveValue;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.StringJoiner;
 
 @Component
 public class UserRepository implements CrudRepository<UserEntity, Long> {
@@ -32,6 +34,8 @@ public class UserRepository implements CrudRepository<UserEntity, Long> {
     @Autowired
     private TableClient tableClient;
 
+    private final TxControl<TxControl.TxSerializableRw> txControl = TxControl.serializableRw().setCommitTx(true);
+
     @Override
     public <S extends UserEntity> S save(S entity) {
         String query
@@ -46,7 +50,6 @@ public class UserRepository implements CrudRepository<UserEntity, Long> {
                 "$uName", PrimitiveValue.newText(entity.getuName()),
                 "$uDiscriminator", PrimitiveValue.newUint8(entity.getuDiscriminator())
         );
-        TxControl txControl = TxControl.serializableRw().setCommitTx(true);
         DataQueryResult result = sessionRetryContext.supplyResult(session -> session.executeDataQuery(query, txControl, params)).join().getValue();
         System.out.println(result);
 
@@ -55,7 +58,7 @@ public class UserRepository implements CrudRepository<UserEntity, Long> {
 
     @Override
     public <S extends UserEntity> Iterable<S> saveAll(Iterable<S> entities) {
-        ArrayList<UserEntity> savedEntities = new ArrayList<>();
+        List<UserEntity> savedEntities = new ArrayList<>();
         for (UserEntity entity : entities) {
             String query
                     = "DECLARE $uId AS Uint64; " +
@@ -69,7 +72,6 @@ public class UserRepository implements CrudRepository<UserEntity, Long> {
                     "$uName", PrimitiveValue.newText(entity.getuName()),
                     "$uDiscriminator", PrimitiveValue.newUint8(entity.getuDiscriminator())
             );
-            TxControl txControl = TxControl.serializableRw().setCommitTx(true);
             DataQueryResult result = sessionRetryContext.supplyResult(session -> session.executeDataQuery(query, txControl, params)).join().getValue();
             savedEntities.add(entity);
         }
@@ -84,7 +86,6 @@ public class UserRepository implements CrudRepository<UserEntity, Long> {
         Params params = Params.of(
                 "$uId", PrimitiveValue.newUint64(aLong)
         );
-        TxControl<TxControl.TxSerializableRw> txControl = TxControl.serializableRw().setCommitTx(true);
         Result<DataQueryResult> result = sessionRetryContext.supplyResult(session -> session.executeDataQuery(query, txControl, params)).join();
         ResultSetReader rs = result.getValue().getResultSet(0);
         if (result.isSuccess()) {
@@ -106,7 +107,6 @@ public class UserRepository implements CrudRepository<UserEntity, Long> {
         Params params = Params.of(
                 "$uId", PrimitiveValue.newUint64(aLong)
         );
-        TxControl<TxControl.TxSerializableRw> txControl = TxControl.serializableRw().setCommitTx(true);
         DataQueryResult result = sessionRetryContext.supplyResult(session -> session.executeDataQuery(query, txControl)).join().getValue();
 
         return result.getResultSet(0).getRowCount() > 0;
@@ -116,10 +116,9 @@ public class UserRepository implements CrudRepository<UserEntity, Long> {
     public Iterable<UserEntity> findAll() {
         String query
                 = "SELECT * from " + database + "; ";
-        TxControl<TxControl.TxSerializableRw> txControl = TxControl.serializableRw().setCommitTx(true);
         DataQueryResult result = sessionRetryContext.supplyResult(session -> session.executeDataQuery(query, txControl)).join().getValue();
         ResultSetReader rs = result.getResultSet(0);
-        ArrayList<UserEntity> userEntities = new ArrayList<>();
+        List<UserEntity> userEntities = new ArrayList<>();
         while (rs.next()) {
             userEntities.add(new UserEntity(
                     rs.getColumn("uId").getUint64(),
@@ -134,32 +133,39 @@ public class UserRepository implements CrudRepository<UserEntity, Long> {
     @Override
     public Iterable<UserEntity> findAllById(Iterable<Long> longs) {
 
+        StringBuilder query = new StringBuilder();
+        Params params = Params.empty();
+        StringJoiner idPlaceholders = new StringJoiner(",");
+        int paramID = 1;
         for (Long uId : longs) {
-            String query
-                    = "SELECT * from " + database + " WHERE uId = $uId; ";
-            Params params = Params.of(
-                    "$uId", PrimitiveValue.newUint64(uId)
-            );
-            TxControl<TxControl.TxSerializableRw> txControl = TxControl.serializableRw().setCommitTx(true);
-            DataQueryResult result = sessionRetryContext.supplyResult(session -> session.executeDataQuery(query, txControl)).join().getValue();
-            ResultSetReader rs = result.getResultSet(0);
-            ArrayList<UserEntity> userEntities = new ArrayList<>();
-            while (rs.next()) {
-                userEntities.add(new UserEntity(
-                        rs.getColumn("uId").getUint64(),
-                        rs.getColumn("uName").getText(),
-                        rs.getColumn("uDiscriminator").getUint8()
-                ));
-            }
-        }
+            String parameterName = "$uId" + paramID;
+            idPlaceholders.add(parameterName);
+            params.put(parameterName, PrimitiveValue.newUint64(uId));
 
+            query.append("DECLARE ").append(parameterName).append(" AS Uint64;\n");
+        }
+        query.append("SELECT * from ").append(database).append(" WHERE uId IN (").append(idPlaceholders).append("); ");
+
+        DataQueryResult result = sessionRetryContext.supplyResult(session -> session.executeDataQuery(query.toString(), txControl, params)).join().getValue();
+        ResultSetReader rs = result.getResultSet(0);
+        List<UserEntity> userEntities = new ArrayList<>();
+        while (rs.next()) {
+            userEntities.add(new UserEntity(
+                    rs.getColumn("uId").getUint64(),
+                    rs.getColumn("uName").getText(),
+                    rs.getColumn("uDiscriminator").getUint8()
+            ));
+        }
 
         return userEntities;
     }
 
     @Override
     public long count() {
-        return 0;
+        String query = "SELECT COUNT(*) FROM " + database + "; ";
+        DataQueryResult result = sessionRetryContext.supplyResult(session -> session.executeDataQuery(query.toString(), txControl)).join().getValue();
+        ResultSetReader rs = result.getResultSet(0);
+        return rs.getColumn("column0").getUint64();
     }
 
     @Override
@@ -177,21 +183,40 @@ public class UserRepository implements CrudRepository<UserEntity, Long> {
 
     @Override
     public void delete(UserEntity entity) {
-
+        deleteById(entity.getUId());
     }
 
     @Override
     public void deleteAllById(Iterable<? extends Long> longs) {
+        StringBuilder query = new StringBuilder();
+        Params params = Params.empty();
+        StringJoiner idPlaceholders = new StringJoiner(",");
+        int paramID = 1;
+        for (Long uId : longs) {
+            String parameterName = "$uId" + paramID;
+            idPlaceholders.add(parameterName);
+            params.put(parameterName, PrimitiveValue.newUint64(uId));
 
+            query.append("DECLARE ").append(parameterName).append(" AS Uint64;\n");
+        }
+        query.append("DELETE FROM ").append(database).append(" WHERE uId IN (").append(idPlaceholders).append("); ");
+
+        sessionRetryContext.supplyResult(session -> session.executeDataQuery(query.toString(), txControl, params)).join().getValue();
     }
 
     @Override
     public void deleteAll(Iterable<? extends UserEntity> entities) {
-
+        List<Long> uIDs = new ArrayList<Long>();
+        for (UserEntity entity : entities) {
+            uIDs.add(entity.getUId());
+        }
+        deleteAllById(uIDs);
     }
 
     @Override
     public void deleteAll() {
-
+        String query = "DELETE FROM " + database;
+        TxControl<TxControl.TxSerializableRw> txControl = TxControl.serializableRw().setCommitTx(true);
+        DataQueryResult result = sessionRetryContext.supplyResult(session -> session.executeDataQuery(query, txControl)).join().getValue();
     }
 }
